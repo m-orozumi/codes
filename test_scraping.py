a265e9618e82c01da5290762166e49a65ee5dc37
moro <rena.morozumi@zdh.co.jp>
2024-05-08 15:26:03 +0900

    Added

--- test_scraping.py
+++ test_scraping.py
@@ -1,13 +1,7 @@
 from scrape_code import retrieve
 
-
 def test_scraping_function():
     # テストの実装
-    url = "https://zdh.stagingbridge.net/"
-
-    result = retrieve(url)
-
-    assert result == {
-        'title': ["xxx"],
-        'link': ["yyyy"],
-    }
+    html = "https://zdh.stagingbridge.net/"
+    result = retrieve(html)
+    assert result is not None
