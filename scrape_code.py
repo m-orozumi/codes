c0593b80cb8bc03c40d73297262fe10d2fd21cd2
moro <rena.morozumi@zdh.co.jp>
2024-05-08 09:36:47 +0900

    Added

--- scrape_code.py
+++ scrape_code.py
@@ -24,7 +24,7 @@ def test_scraping_function():
 ## スクレイピング ##
 # 対象サイトのURLを格納する
 #url = "https://zdh.stagingbridge.net/"
-url = "https://zdh.stagingbridge.net/?s=ZEAL"
+url = "https://zdh.stagingbridge.net/?s=AWS"
 
 # requestsを使用してhtmlを取得　→変数reqに格納
 #req = requests.get(url, params={'q': 'AWS'})
